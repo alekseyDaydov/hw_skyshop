d4fc2fb50c4e97d6cb54573ab6c08526d930e30a
Aleksey <d_alesha@rambler.ru>
2025-08-19 17:51:52 +0300

    Добавление пакетов и классов

---
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -0,0 +1,18 @@
+package org.skypro.skyshop.basket;
+
+import org.skypro.skyshop.product.Product;
+
+public class ProductBasket {
+    private Product product;
+    private int count;
+
+    void addProduct(Product product) {
+
+    }
+
+    int getAllAmount() {
+        int amount = 0;
+        return amount;
+    }
+
+}
---
+++ src/org/skypro/skyshop/product/Product.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.product;
+
+public class Product {
+    private String name;
+    private int amount;
+
+    public Product(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
