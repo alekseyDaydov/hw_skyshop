4af3cbd5e6c54324141f76e7cbcac4835b3157b0
d_alesha <d_alesha@rambler.ru>
2025-08-19 21:58:21 +0300

    Добавление методов

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -3,16 +3,57 @@ package org.skypro.skyshop.basket;
 import org.skypro.skyshop.product.Product;
 
 public class ProductBasket {
-    private Product product;
-    private int count;
+    private Product[] productBasket = new Product[5];
 
-    void addProduct(Product product) {
-
+    public void addProduct(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException("Передано пустое значение");
+        }
+        boolean isAddProduct = true;
+        for (int i = 0; i < productBasket.length && isAddProduct; i++) {
+            if (productBasket[i] == null) {
+                productBasket[i] = product;
+                isAddProduct = false;
+            }
+        }
+        if (isAddProduct) {
+            System.out.println("Невозможно добавить продукт " + product.getName());
+        }
     }
 
-    int getAllAmount() {
-        int amount = 0;
-        return amount;
+    public int getAmountBasket() {
+        int result = 0;
+        for (int i = 0; i < productBasket.length && productBasket[i] != null; i++) {
+            result += productBasket[i].getAmount();
+        }
+        return result;
     }
 
+    public void printProductBasket() {
+        int result = 0;
+        for (int i = 0; i < productBasket.length && productBasket[i] != null; i++) {
+            System.out.println(productBasket[i]);
+            result += productBasket[i].getAmount();
+        }
+        if (result != 0) {
+            System.out.println("Итого: <" + result + ">");
+        } else {
+            System.out.println("В корзине пусто");
+        }
+    }
+
+    public boolean isCheckProductBasket(Product product) {
+        for (int i = 0; i < productBasket.length && productBasket[i] != null; i++) {
+            if (productBasket[i].getName() == product.getName()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void clearBasket() {
+        for (int i = 0; i <= productBasket.length && productBasket[i] != null; i++) {
+            productBasket[i] = null;
+        }
+    }
 }
--- src/org/skypro/skyshop/product/Product.java
+++ src/org/skypro/skyshop/product/Product.java
@@ -5,6 +5,9 @@ public class Product {
     private int amount;
 
     public Product(String name, int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Цена товара не может быть меньше нуля");
+        }
         this.name = name;
         this.amount = amount;
     }
@@ -15,5 +18,10 @@ public class Product {
 
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "<" + name + "> : <" + amount + ">";
     }
 }
